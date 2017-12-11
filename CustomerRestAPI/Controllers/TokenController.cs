d6544753c0c56b8f98834b569fb56a6b1707257b
lbilde <ljb@easv.dk>
2017-12-11 11:04:20 +0100

    adding claims

--- CustomerRestAPI/Controllers/TokenController.cs
+++ CustomerRestAPI/Controllers/TokenController.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using CustomerAppBLL;
 using CustomerAppBLL.BusinessObjects;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 
@@ -14,7 +15,9 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace CustomerRestAPI.Controllers
 {
-    [Route("/token")]
+    [EnableCors("MyPolicy")]
+    [Produces("application/json")]
+    [Route("/api/token")]
     public class TokenController : Controller
     {
         IBLLFacade _facade;
@@ -26,7 +29,7 @@ namespace CustomerRestAPI.Controllers
         public IActionResult Create(string username, string password)
         {
             var user = IsValidUserAndPasswordCombination(username, password);
-            if(!string.IsNullOrEmpty(username) && user != null){
+            if(user != null && !string.IsNullOrEmpty(username)){
                 var token = GenerateToken(user);
                 return new ObjectResult(token);
             }
@@ -40,17 +43,17 @@ namespace CustomerRestAPI.Controllers
             return userFound;
         }
 
-        private string GenerateToken(UserBO user)
+        private IActionResult GenerateToken(UserBO user)
         {
             var claims = new List<Claim>
             {
-                new Claim(ClaimTypes.Name, user.Username),
+                new Claim("username", user.Username),
                 new Claim(JwtRegisteredClaimNames.Nbf, new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds().ToString()),
                 new Claim(JwtRegisteredClaimNames.Exp, new DateTimeOffset(DateTime.Now.AddDays(1)).ToUnixTimeSeconds().ToString()),
             };
 
             if (user.Role == "Administrator")
-                claims.Add(new Claim(ClaimTypes.Role, "Administrator"));
+                claims.Add(new Claim("role", "Administrator"));
 
 
             var token = new JwtSecurityToken(
@@ -59,7 +62,7 @@ namespace CustomerRestAPI.Controllers
                                              SecurityAlgorithms.HmacSha256)),
                 new JwtPayload(claims));
 
-            return new JwtSecurityTokenHandler().WriteToken(token);
+            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
         }
     }
 
